865daa625d31c55000a11133f81467f8deb980fb
aliensen36 <142628769+aliensen36@users.noreply.github.com>
2024-04-21 11:58:16 +0300

    1
    1

--- Points_of_view/backend/admin.py
+++ Points_of_view/backend/admin.py
@@ -2,13 +2,17 @@ from django.contrib import admin
 from .models import Send_idea, EnglishContent
 
 class SendIdeaAdmin(admin.ModelAdmin):
-    list_display = ('name', 'phone', 'email', 'created', 'is_published')  # Поля, отображаемые в списке
-    list_filter = ('is_published', 'created')  # Фильтры на странице списка
-    search_fields = ('name', 'phone', 'email')  # Поля, по которым можно проводить поиск
+    list_display = ('name', 'phone', 'email', 'created', 'is_published')
+    list_filter = ('is_published', 'created')
+    search_fields = ('name', 'phone', 'email')
 
 class EnglishContentAdmin(admin.ModelAdmin):
-    list_display = ('about', 'projects', 'works', 'team', 'contacts')  # Поля, отображаемые в списке
-    search_fields = ('about', 'projects', 'works')  # Поля, по которым можно проводить поиск
+    list_display = ('about', 'projects', 'works', 'team', 'contacts',
+                    'hero_subtitle', 'hero_btn', 'who_subtitle', 'who_p1', 'who_p2',
+                    'who_p3', 'projects_subtitle', 'projects_p1', 'projects_p2',
+                    'team_subtitle', 'team_p1')
+    search_fields = ('about', 'projects', 'works', 'team', 'contacts')
+    readonly_fields = ('created',)
 
 admin.site.register(Send_idea, SendIdeaAdmin)
 admin.site.register(EnglishContent, EnglishContentAdmin)
